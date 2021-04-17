f8c1c34df4cbcb19317c2697adc15d7bd8f893e1
Bruno Nascimento <bruno.alves24@fatecitu.edu.br>
2021-04-17 19:38:47 -0300

    Ribbon, load balancing

--- hr-payroll/src/main/java/io/github/brunoalves24/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/io/github/brunoalves24/hrpayroll/service/PaymentService.java
@@ -2,31 +2,22 @@ package io.github.brunoalves24.hrpayroll.service;
 
 import io.github.brunoalves24.hrpayroll.entity.Payment;
 import io.github.brunoalves24.hrpayroll.entity.Worker;
+import io.github.brunoalves24.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
-    private final RestTemplate restTemplate;
-
-    @Value("${hr-worker.host}")
-    private String workerHost;
+    private final WorkerFeignClient workerFeignClient;
 
     @Autowired
-    public PaymentService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public PaymentService(WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker =  workerFeignClient.findById(workerId).getBody();
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
--- hr-worker/src/main/java/io/github/brunoalves24/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/io/github/brunoalves24/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package io.github.brunoalves24.hrworker.resources;
 
 import io.github.brunoalves24.hrworker.entity.Worker;
 import io.github.brunoalves24.hrworker.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +20,14 @@ import java.util.Optional;
 @RequestMapping("/workers")
 public class WorkerResource {
     private final WorkerRepository workerRepository;
+    private final Environment environment;
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
     @Autowired
-    public WorkerResource(WorkerRepository workerRepository) {
+    public WorkerResource(WorkerRepository workerRepository, Environment environment) {
         this.workerRepository = workerRepository;
+        this.environment = environment;
     }
 
     @GetMapping
@@ -31,8 +38,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
-        Optional<Worker> worker = workerRepository.findById(id);
+        logger.info("PORT = " + environment.getProperty("local.server.port"));
 
+        Optional<Worker> worker = workerRepository.findById(id);
         if(worker.isPresent())
             return ResponseEntity.ok(worker.get());
 
