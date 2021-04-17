1969b38a745a31f9010e9d680d6b7085055ab1d2
Bruno Nascimento <bruno.alves24@fatecitu.edu.br>
2021-04-17 15:29:51 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/io/github/brunoalves24/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoalves24.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
